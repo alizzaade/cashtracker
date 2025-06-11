a3675150353d682817101d1054f33f69b2c022ff
umut <alizzaade@users.noreply.github.com>
2025-06-11 20:28:00 +0200

    Add Expenses page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace cashTracker.Controllers
         {
             return View();
         }
+        public IActionResult Expenses()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
