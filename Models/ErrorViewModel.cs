273471da713f7c93c933783690cb8348d1ce4ab3
umut <alizzaade@users.noreply.github.com>
2025-06-11 20:10:16 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cashTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
