5b1a9f22d13b1162c018dc087f976d81de929984
umut <alizzaade@users.noreply.github.com>
2025-06-20 18:44:16 +0200

    Add Register method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using cashTracker.Models;
+using cashTracker.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace cashTracker.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -12,6 +24,34 @@ namespace cashTracker.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                } else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(model);
+                }
+            }
+            return View(model);
         }
 
         public IActionResult VerifyEmail()
